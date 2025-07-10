acf6fd313f331a74721a49413ece1d724ce28b94
Gopi <gopichand.t@innovitegra.com>
2025-07-10 23:33:50 +0530

    Integrated deployed backend API into frontend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,3 @@
-import axios from 'axios';
-
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://docmanager-backend-187y.onrender.com/api',
 });
-
-export default API;
