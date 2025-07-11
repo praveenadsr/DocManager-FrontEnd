3b0988e4aab4839d68a7392873fe303ca1f963b6
Gopi <gopichand.t@innovitegra.com>
2025-07-11 10:23:58 +0530

    added backned deplyed link in fronted instead of localhost

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const API = axios.create({
 
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
 
-// baseURL: 'https://docmanager-backend-187y.onrender.com/api',
-
+baseURL: 'https://docmanager-backend-187y.onrender.com/api',
+ 
 
 
 
